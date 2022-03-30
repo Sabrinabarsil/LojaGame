032e37e320a23359c88cd9561a9f7dcbc360bf12
Sabrinabarsil <barsilsabrina@gmail.com>
2022-03-30 15:25:46 -0300

    CRUD I

---
+++ src/main/java/com/generation/lojagame/model/Produto.java
@@ -0,0 +1,126 @@
+package com.generation.lojagame.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo Nome é Obrigatório!")
+	private String nome;
+
+	@Size(max = 200)
+	private String descricao;
+
+	@NotBlank(message = "O atributo console é Obrigatório!")
+	private String console;
+
+	private int quantidade;
+
+	@UpdateTimestamp
+	private LocalDateTime dataLancamento;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@NotBlank(message = "Preço é obrigatório!")
+	@Positive(message = "O preço deve ser maior que zero!")
+	private BigDecimal preco;
+
+	private String foto;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public LocalDateTime getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(LocalDateTime dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+}
---
+++ src/main/java/com/generation/lojagame/repository/ProdutoRepository.java
@@ -0,0 +1,19 @@
+package com.generation.lojagame.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.lojagame.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+	
+	public List<Produto> findAllByPrecoGreaterThanOrderByPreco(BigDecimal preco);
+	
+	public List<Produto> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
+}
